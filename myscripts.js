e4fc4c20810ff37208a80c07493b6e74b3cbaaae
DeMoravie <32600414+DeMoravie@users.noreply.github.com>
2018-07-15 13:58:39 +0200

    fifth upload

--- myscripts.js
+++ myscripts.js
@@ -27,6 +27,45 @@ var img22 = document.getElementById('myImg22');
 var img23 = document.getElementById('myImg23');
 var img24 = document.getElementById('myImg24');
 var img25 = document.getElementById('myImg25');
+var img26 = document.getElementById('myImg26');
+var img27 = document.getElementById('myImg27');
+var img28 = document.getElementById('myImg28');
+var img29 = document.getElementById('myImg29');
+var img30 = document.getElementById('myImg30');
+var img31 = document.getElementById('myImg31');
+var img32 = document.getElementById('myImg32');
+var img33 = document.getElementById('myImg33');
+var img34 = document.getElementById('myImg34');
+var img35 = document.getElementById('myImg35');
+var img35 = document.getElementById('myImg35');
+var img36 = document.getElementById('myImg36');
+var img37 = document.getElementById('myImg37');
+var img38 = document.getElementById('myImg38');
+var img39 = document.getElementById('myImg39');
+var img40 = document.getElementById('myImg40');
+var img41 = document.getElementById('myImg41');
+var img42 = document.getElementById('myImg42');
+var img43 = document.getElementById('myImg43');
+var img44 = document.getElementById('myImg44');
+var img45 = document.getElementById('myImg45');
+var img46 = document.getElementById('myImg46');
+var img47 = document.getElementById('myImg47');
+var img48 = document.getElementById('myImg48');
+var img49 = document.getElementById('myImg49');
+var img50 = document.getElementById('myImg50');
+var img51 = document.getElementById('myImg51');
+var img52 = document.getElementById('myImg52');
+var img53 = document.getElementById('myImg53');
+var img54 = document.getElementById('myImg54');
+var img55 = document.getElementById('myImg55');
+var img56 = document.getElementById('myImg56');
+var img57 = document.getElementById('myImg57');
+var img58 = document.getElementById('myImg58');
+var img59 = document.getElementById('myImg59');
+var img60 = document.getElementById('myImg60');
+var img61 = document.getElementById('myImg61');
+var img62 = document.getElementById('myImg62');
+var img63 = document.getElementById('myImg63');
 var modalImg = document.getElementById("modalImg");
 var captionText = document.getElementById("caption");
 
@@ -34,150 +73,448 @@ img1.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img2.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img3.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img4.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img5.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img6.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img7.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img8.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img9.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img10.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img11.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img12.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img13.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img14.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img15.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img16.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img17.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img18.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img19.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img20.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});
 }
 
 img21.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img22.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img23.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img24.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});
 }
 
 img25.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img26.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});		
+}
+
+img27.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img28.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img29.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img30.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img31.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img32.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img33.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img34.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img35.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});		
+}
+
+img36.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img37.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img38.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img39.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
     captionText.innerHTML = this.title;		
+	$("html").css({"overflow":"hidden"});	
+}
+
+img40.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img41.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img42.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img43.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img44.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img45.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});		
+}
+
+img45.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});		
+}
+
+img46.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img47.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img48.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img49.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;		
+	$("html").css({"overflow":"hidden"});	
+}
+
+img50.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img51.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img52.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img53.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img54.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img55.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;		
+	$("html").css({"overflow":"hidden"});	
+}
+
+img56.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img57.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img58.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img59.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img60.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;
+	$("html").css({"overflow":"hidden"});	
+}
+
+img61.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img62.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
+}
+
+img63.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.title;	
+	$("html").css({"overflow":"hidden"});	
 }
 
 // Get the <span> element that closes the modal
@@ -186,7 +523,8 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
-  document.body.style.overflow = "visible";	//show background scroll bar after pushing close button of an image pop up
+  //document.body.style.overflow = "scroll";	//show background scroll bar after pushing close button of an image pop up
+  $("html").css({"overflow":"scroll"});
 }
 
 function filterSelection(c) {	//Definition of function filterSelection
