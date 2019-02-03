43b8de9e20958cbd82db9dce933fe0b131690fe4
DeMoravie <32600414+DeMoravie@users.noreply.github.com>
2019-02-03 14:46:58 +0100

    vdfv
    33rff3

--- myscripts.js
+++ myscripts.js
@@ -534,3 +534,27 @@ function closeModal() {
   //document.body.style.overflow = "scroll";	//show background scroll bar after pushing close button of an image pop up
   $("html").css({"overflow":"scroll"});
 }
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropDown() {
+  document.getElementById('myDropDown').classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
