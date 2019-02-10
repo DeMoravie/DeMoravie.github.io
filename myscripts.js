6e3b8fb3b10e331baadda1fcaf5e798466d65331
DeMoravie <32600414+DeMoravie@users.noreply.github.com>
2019-02-10 01:09:11 +0100

    sdvsdv
    ewefw

--- myscripts.js
+++ myscripts.js
@@ -558,3 +558,28 @@ window.onclick = function(event) {
 
 
 
+
+function cacheImages(array)
+{
+    if (!cacheImages.list) {
+        cacheImages.list = [];
+    }
+    var list = cacheImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+cacheImages(["1-compressor-resized.jpg", "2-compressor-resized.jpg", "3-compressor-resized.jpg"]);
+cacheImages(["1-compressor-25-min.jpg", "1-compressor-25-min.jpg", "1-compressor-25-min.jpg"]);
+
